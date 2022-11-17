e328a35a267cd44c04dd4ac762fa940ac2d19506
Elle <118459583+myff1n@users.noreply.github.com>
2022-11-17 11:53:58 -0700

    More work done. Almost finished

--- python/madlibs.py
+++ python/madlibs.py
@@ -4,12 +4,18 @@ import random
 
 #user prompt
 print("Give me an ADJECTIVE")
-adj1 = input("")
+adj = input("")
 print("Now give me a FOOD")
 food = input("")
-
+print("Past tense VERB?")
+past_verb = input()
+print("What about a PERSON?")
+person = input("")
 
 print("Generating story...")
 time.sleep(random.randint(1, 3))
 
-print("It was a cold,", adj1, ", winter day. I woke up to the smell of", food, "cooking downstairs.")
+print("It was a cold,", adj, ", winter day."
+"I woke up to the smell of", food, "cooking downstairs."
+"I", past_verb, "down the stairs to help with the", food, "."
+"My", person, )
