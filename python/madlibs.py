161c96cde4755a930ee8d98afdb25c8a74026107
Elle <118459583+myff1n@users.noreply.github.com>
2022-11-17 11:27:56 -0700

    Update madlibs.py

--- python/madlibs.py
+++ python/madlibs.py
@@ -1,1 +1,15 @@
 
+import time
+import random
+
+#user prompt
+print("Give me an ADJECTIVE")
+adj1 = input("")
+print("Now give me a FOOD")
+food = input("")
+
+
+print("Generating story...")
+time.sleep(random.randint(1, 3))
+
+print("It was a cold,", adj1, ", winter day. I woke up to the smell of", food, "cooking downstairs.")
