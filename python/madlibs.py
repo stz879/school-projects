7e1cad26fd89731f7adad9bd4948ef8110fb7003
Elle <118459583+myff1n@users.noreply.github.com>
2022-12-12 07:51:30 -0700

    Changed some grammar and variable names

--- python/madlibs.py
+++ python/madlibs.py
@@ -1,3 +1,6 @@
+#user prompt
+print("Give me an ADJECTIVE")
+adj_1 = input("")
 
 import time
 import random
@@ -9,13 +12,13 @@ time.sleep(random.randint(1, 5))
 
 #user prompts
 print("Give me an ADJECTIVE")
-adj = input("")
+adjective = input("")
 
 print("Now give me a FOOD")
 food = input("")
 
 print("Past tense VERB?")
-past_verb = input("")
+past_tense_verb = input("")
 
 print("What about a PERSON?")
 person = input("")
@@ -23,23 +26,23 @@ person = input("")
 print("Ok, now a RELATIVE")
 relative = input("")
 
-print("Give me a NOUN (singular)")
-noun1 = input("")
+print("Give me a SINGULAR NOUN")
+first_noun = input("")
 
 print("Alright, now a LIQUID")
 liquid = input("")
 
 print("A VERB ending in -ING")
-verb_ing1 = input("")
+verb-ing = input("")
 
-print("I need a NOUN (plural")
-noun_plural = input("")
+print("I need a PLURAL NOUN")
+plural_noun = input("")
 
 print("A piece of FURNITURE")
 furniture = input("")
 
 print("Last one! Another VERB ending in ING")
-verb_ing2 = input("")
+second_verb-ing = input("")
 
 #"wait time" - waits between 1 to 3 seconds
 print("Generating story...")
@@ -47,10 +50,11 @@ time.sleep(random.randint(1, 3))
 
 
 #print story
-print(f"It was a cold, {adj}, winter day.")
+print(f"It was a cold, {adjective}, winter day.")
 print(f"I woke up to the smell of {food} cooking downstairs.")
-print(f"I {past_verb} down the stairs to help with the {food}.")
-print(f"My {person} told me to see if my {relative} needed a fresh {noun1}.")
-print(f"So I carried a tray of glasses full of {liquid} into the {verb_ing1} room.")
-print(f"When I came into the {verb_ing1} room, I couldn't believe my eyes!")
-print(f"There were {noun_plural} {verb_ing2} on the {furniture}!")
+print(f"I {past_tense_verb} down the stairs to help with the {food}.")
+print(f"My {person} told me to see if my {relative} needed a fresh {first_noun}.")
+print(f"So I carried a tray of glasses full of {liquid} into the {verb-ing} room.")
+print(f"When I came into the {verb-ing} room, I couldn't believe my eyes!")
+print(f"There were {plural_noun} {second_verb-ing} on the {furniture}!")
+
