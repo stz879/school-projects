f3e5255ece7b5c3f8a6aac748193ae7dfea00fc2
Elle <118459583+myff1n@users.noreply.github.com>
2023-01-05 10:13:21 -0700

    Update compound-interest.py

--- python/compound-interest.py
+++ python/compound-interest.py
@@ -5,8 +5,11 @@ import math
 
 #prompt user
 print("Hello! Welcome to the Compound Interest Program!")
-print("Interest is 6% per year")
+time.sleep(1)
 print("")
+print("")
+print("How much interest? (as a decimal, ex 0.06 = 6%")
+interest = float(input(""))
 print("How much money will you deposit?")
 #save deposit as a variable, and make it a float
 depInput = input("")
@@ -17,20 +20,31 @@ yrsInput = input("")
 yrs = int(yrsInput)
 
 #calculate 1st year. done for variable convenience
-year1 = (dep * 0.06) + dep
+year1 = (dep * interest) + dep
 
 i = 1
 finalAmount = year1
 
 #loop for each year
 while i < yrs:
-    finalAmount = (finalAmount * 0.06) + finalAmount
+    finalAmount = (finalAmount * interest) + finalAmount
     i += 1
+    print(f"Year {i}")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("How much money would you like to add to your deposit?")
+    print("Type 0 if you don't want to add any more money")
+    x = input("")
+    float(x)
+    finalAmount += x
 
 
 
 #added "load time" just for fun
 print("Calculating...")
-time.sleep(random.randint(1, 5))
+time.sleep(random.randint(1, 3))
 #                                                v make the end result at least 4 characters, with 2 numbers past the decimal point
-print(f"Your total, after {yrs} years, is $", '{:04.2f}'.format(finalAmount))
+print(f"Your total, after {yrs} years, is $", '{:01.2f}'.format(finalAmount))
