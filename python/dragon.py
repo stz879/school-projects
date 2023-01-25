175446f93f33db52027f53a07cf1db127f12ce3f
Elle <118459583+myff1n@users.noreply.github.com>
2023-01-25 09:50:21 -0700

    .

---
+++ python/dragon.py
@@ -0,0 +1,11 @@
+import turtle
+#import time
+s = turtle.Screen()
+
+#set screen size, turtle speed, background color, turtle pen/fill color, and turtle shape
+s.screensize(600, 600)
+turtle.speed('fastest')
+s.bgcolor('white')
+turtle.color('black', 'red')
+turtle.shape('classic')
+
