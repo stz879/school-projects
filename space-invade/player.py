056a4d94bb616b4b6adbce7d31b2192214fc9701
presley <121964468+stz879@users.noreply.github.com>
2023-01-26 10:50:25 -0700

    Update player.py

--- space-invade/player.py
+++ space-invade/player.py
@@ -1,5 +1,8 @@
 #listen for variable to be true
 #player spawn when variable set to true
+player = turtle.Turtle()
+player.shape("player.gif")
+player.penup()
 
 #set player hp and damage
     #player starting hp = 10
@@ -9,14 +12,19 @@ player_dmg = 1
 
 #set score to 0
 score = 0
+player.speed(0)
+playerspeed = 15
 
 
 #player controls
     #left arrow/A pressed, move left
     #right arrow/D pressed, more right
     #space pressed, shoot
+player.setposition(0,-250)
+player.setheading(90)
 
 #if player hp hits 0
     #show explosion
     #wait 3 seconds
     #go to game start screen
+    player = turtle.Turtle()
