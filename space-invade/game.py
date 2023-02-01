49ddc46439ff751045da5fcda636baa7c4ceab22
Elle <118459583+myff1n@users.noreply.github.com>
2023-02-01 10:30:36 -0700

    .

--- space-invade/game.py
+++ space-invade/game.py
@@ -5,74 +5,81 @@ import os
 import winsound
 
 wn = turtle.Screen()
+wn.register_shape("spaceship.gif")
 
-#splash screen, "insert coin/press any button" prompt
-    #screen switches when button pressed
+#draw screen, music begins
+wn.title("spase ivaders !!")
+wn.bgcolor("#db5ef7")
+wn.bgpic("spase.gif")
+wn.setup(width=1200, height=831)
+wn.tracer(0)
+
 
 def game():
-    time.sleep(random.randint(1,2))
-
-    #draw screen, music begins
-    
-    wn.title("spase ivaders !!")
-    wn.bgcolor("#db5ef7")
-    wn.bgpic("spase.gif")
-
-    wn.setup(width=1200, height=831)
-    wn.tracer(0)
+    #set score to 0
+    score = 0
+    high_score = 0
 
     pen = turtle.Turtle()
     pen.speed(0)
     pen.hideturtle()
     pen.color("white")
     pen.goto(-200, 330)
-    pen.write("Score: 0", align="center", font=("candara", 24, "bold"))
+    pen.write(f"Score: {score}", align="center", font=("Comic Sans MS", 24, "bold"))
     pen.goto(200, 330)
-    pen.write("High Score: 0", align="center", font=("candara", 24, "bold"))
+    pen.write(f"High Score: {high_score}", align="center", font=("Comic Sans MS", 24, "bold"))
 
-    #set spawn variables to true for n seconds
+    #splash screen, "insert coin/press any button" prompt
+    #screen switches when button pressed
 
     #spawn barriers
         #2 barriers, one on each side of the screen
         #barriers block all bullets
         #barriers never disappear
 
-    #player spawn when variable set to true
+    #player spawn
     player = turtle.Turtle()
+    turtle.mode("logo")
+    player.speed("slowest")
+    player.shape("circle")
+    player.pencolor("black")
+    player.penup()
+    player.resizemode("user")
+    player.shapesize(50, 50, 5)
+    player.showturtle()
+    player.goto(0, -300)
+    player.seth(0)
 
-    #set player hp and damage
+    #set player hp
     player_hp = 3
-        #player starting hp = 3
-    #player_dmg = 1
-        #player starting damage = 1
+        
 
-    #set score to 0
-
-    #player controlfunctions
+    #player control functions
     def shoot():
         #function for player shooting
         pass
 
     def move_left():
-        #function for player moving left
-        pass
+        player.seth(270)
+        player.backward(5)
 
     def move_right():
-        #function for player moving right
-        pass
+        player.seth(90)
+        player.forward(5)
 
 
     wn.listen()
+
     #left arrow/A pressed, move left
-    player.onkeypress(move_left, "a")
-    player.onkeypress(move_left, "Left")
+    turtle.onkeypress(move_left, "a")
+    turtle.onkeypress(move_left, "Left")
 
     #right arrow/D pressed, move right
-    player.onkeypress(move_right, "d")
-    player.onkeypress(move_right, "Right")
+    turtle.onkeypress(move_right, "d")
+    turtle.onkeypress(move_right, "Right")
 
     #space pressed, shoot
-    player.onkeypress(shoot, "space")
+    turtle.onkeypress(shoot, "space")
 
     #if player hp hits 0
     if player_hp == 0:
@@ -84,5 +91,5 @@ def game():
         game()
 
 game()
-wn.mainloop()
 
+wn.mainloop()
