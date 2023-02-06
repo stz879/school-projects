faf226d93a6fc64d0598357cdc95f6916ed10efc
Elle <118459583+myff1n@users.noreply.github.com>
2023-02-06 10:28:58 -0700

    Update dragon.py

--- python/dragon.py
+++ python/dragon.py
@@ -6,10 +6,10 @@ s = t.Screen()
 t.mode('logo')
 t.hideturtle()
 s.screensize(800, 800)
-t.speed('slow')
+t.speed('fast')
 s.bgcolor('white')
-t.pencolor("black")
-t.fillcolor("red")
+t.pencolor('black')
+t.fillcolor('red')
 t.shape('classic')
 
 
@@ -19,10 +19,11 @@ t.pu()
 t.setpos(80, 0)
 t.seth(270)
 
+#first triangle
 t.pd()
 t.begin_fill()
 t.forward(40)
-t.left(90)
+t.seth(180)
 t.forward(40)
 t.seth(45)
 t.forward(58)
@@ -33,6 +34,7 @@ t.forward(13)
 t.seth(180)
 t.forward(15)
 
+#second triangle
 t.pd()
 t.begin_fill()
 t.forward(50)
@@ -47,6 +49,7 @@ t.forward(15)
 t.seth(180)
 t.forward(10)
 
+#third triangle
 t.pd()
 t.begin_fill()
 t.forward(50)
@@ -58,10 +61,77 @@ t.end_fill()
 
 t.pu()
 t.seth(90)
-t.forward(10)
+t.forward(15)
 
+#fourth triangle
 t.pd()
 t.begin_fill()
+t.forward(90)
+t.seth(240)
+t.forward(70)
+t.seth(319)
+t.forward(48)
+t.end_fill()
+
+
+#left wing
+t.pu()
+t.setpos(-80, 0)
+t.seth(90)
+
+#first triangle
+t.pd()
+t.begin_fill()
+t.forward(40)
+t.seth(180)
+t.forward(40)
+t.seth(315)
+t.forward(58)
+t.end_fill()
+
+t.pu()
+t.forward(13)
+t.seth(180)
+t.forward(15)
+
+#second triangle
+t.pd()
+t.begin_fill()
+t.forward(50)
+t.seth(75)
+t.forward(40)
+t.seth(314)
+t.forward(55)
+t.end_fill()
+
+t.pu()
+t.forward(15)
+t.seth(180)
+t.forward(10)
+
+#third triangle
+t.pd()
+t.begin_fill()
+t.forward(50)
+t.seth(285)
+t.forward(40)
+t.seth(46)
+t.forward(57)
+t.end_fill()
+
+t.pu()
+t.seth(270)
+t.forward(15)
+
+#fourth triangle
+t.pd()
+t.begin_fill()
+t.forward(90)
+t.seth(120)
+t.forward(70)
+t.seth(49)
+t.forward(48)
+t.end_fill()
 
 #close window if clicked
 t.exitonclick()
