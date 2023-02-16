8eefe0b0cfabf8414637d4d65a731017d6dba7a3
Elle <118459583+myff1n@users.noreply.github.com>
2023-02-16 09:58:50 -0700

    Update FizzBuzz.py

--- python/FizzBuzz.py
+++ python/FizzBuzz.py
@@ -1,13 +1,19 @@
+print("Welcome to the FizzBuzz Program!")
+print('What multiples should be "Fizz"?')
+fizz_num = int(input(""))
+print('What multiples should be "Buzz"?')
+buzz_num = int(input(""))
+
 #write numbers 1 - 100
 for i in range(1, 101):
   fizz, buzz = False, False
     
   #check if divisible by 3, write to variable
-  if i % 3 == 0: 
+  if i % fizz_num == 0: 
     fizz = True;
 
   #check if divisible by 5, write to variable
-  if i % 5 == 0: 
+  if i % buzz_num == 0: 
     buzz = True;
   
 #look at variables and output
